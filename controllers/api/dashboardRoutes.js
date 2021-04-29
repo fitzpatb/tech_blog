019bd159a212e5013e1733744159038844eec3d3
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-04-29 13:32:19 -0600

    wont launch

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection.js');
+const sequelize = require('../../config/connection');
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require("../models");
-const sequelize = require("../config/connection.js");
+const sequelize = require("../config/connection");
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
